48210c57432ca4591e21fc8bd39cf42aff1f3b8b
copyrat90 <copyrat90@gmail.com>
2021-05-25 23:52:16 +0900

    Move graphic asset project files to seperate dir

---
+++ graphics_source/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tileset" tilewidth="8" tileheight="8" tilecount="576" columns="24">
+ <image source="tileset.png" width="192" height="192"/>
+</tileset>
