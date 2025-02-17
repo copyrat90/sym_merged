069a2f83e40ef2924e2611e9b7a39765e8a857ff
Gustavo Valiente <gustavo.valiente@protonmail.com>
2025-02-17 11:16:53 +0100

    butano 18.9.0 build fix

--- include/helper_math.h
+++ include/helper_math.h
@@ -7,15 +7,6 @@
 namespace sym::helper::math
 {
 
-constexpr bn::fixed EPSILON = 0.01;
-
-[[nodiscard]] inline bn::fixed operator%(bn::fixed a, int b)
-{
-    bn::fixed result = a - (a / b).floor_integer() * b;
-    BN_ASSERT(-EPSILON <= result && result < b + EPSILON, "operator% is giving wrong result of ", result);
-    return result;
-}
-
 [[nodiscard]] inline constexpr bn::fixed DistanceSquared(bn::fixed_point p1, bn::fixed_point p2)
 {
     return (p2.x() - p1.x()) * (p2.x() - p1.x()) + (p2.y() - p1.y()) * (p2.y() - p1.y());
